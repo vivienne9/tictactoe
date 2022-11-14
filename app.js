137103bf377ed1ed880ac473374b06ecde5bc543
Vivienne Chawande <113947648+vivienne9@users.noreply.github.com>
2022-11-13 23:24:38 -0600

    updated functions

--- app.js
+++ app.js
@@ -1,56 +1,124 @@
+//DOM 
+//Initial state
+//Function to select first player
+//Function to select cell
+//Function to check winning condition
+//Function to display winning text
+//Function to Reset board
+
 const cells = document.querySelectorAll('.cell');
-const getBoard = document.getElementsByClassName('board');
+//const getBoard = document.getElementById('boardId');
 const resetButton = document.getElementById('reset-game');
-
 const endGame = document.getElementById('endGame');
-const endGameText = document.getElementById('win').innerHTML
-const startButton = document.getElementById('startButton');
+const endGameText = document.getElementById('win').innerText
 
-let gameState = {
-    players: ['x','o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
+//Initial state
+let players = ['x','o'];
+let board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ]
-  }
+let turn = '';
 
 //Randomly select first player
-let player = gameState.players;
-let i = Math.floor(Math.random() * player.length);
-let turn = gameState.players[i]; 
+function selectPlayer (){
+    let i = Math.floor(Math.random() * players.length);
+    turn = players[i]; 
+    console.log(turn);
+    return turn;
+}
+selectPlayer();
 
+//Select cell DOM
 for(let i = 0; i < cells.length; i++){
+    cells[i].addEventListener('click', selectCell);
+    cells[i].addEventListener('click', checkWon);
+}
+    
+//Select cell function
+function selectCell (event){
+    let target = event.target;
+    let getId =  target.dataset.id;
+    let getIdx = Math.floor(getId / 3);
+    let getIdy = getId % 3;
 
-    cells[i].addEventListener('click', function(event){
-        target = event.target;
-        if(turn === 'x'){
-            target.innerText = 'x';
-            turn = 'o';    
-        }
-        else if (turn === 'o'){
-            target.innerText = 'o';
-            turn = 'x';  
-        }
-        return turn;
-    }) 
-    continue;    
+    if(target.innerText != ''){
+        return;
+    }
+    if(turn === 'x'){
+        target.innerText = 'x';
+        console.log(getIdx, getIdy);
+        board[getIdx][getIdy] = 'x';
+        console.log(board);
+        turn = 'o';    
+    }
+    else if (turn === 'o'){
+        target.innerText = 'o';
+        console.log(getIdx, getIdy);
+        board[getIdx][getIdy] = 'o';
+        console.log(board);
+        turn = 'x'; 
+    } 
+    return turn, board;
 }
 
+let hasWon = false;
 
-// function win (gameState){
-    // for(let i = 0; i < board.length; i++){
-    //     let row = board[i];
-    //     for (let j = 0; j < board[i].length; j++){
-    //         if(board[i][j] = board[i][j+1]= board[i][j+2]=
-    //     }
+//Check winning condition
+function checkWon (){
+        for(let i = 0; i < 3; i++){
+            if((board[i][0] === board[i][1]) && (board[i][0] === board[i][2]) && (board[i][0] !== '')){
+                hasWon = true;
+                //console.log(hasWon, 'this is first check');
+                playerWon();
+                break;
+                }
+                if((board[0][i] === board[1][i]) && (board[0][i] === board[2][i]) && (board[0][i] !== '')){
+                hasWon = true;
+                //console.log(hasWon, 'this is second check');
+                playerWon();
+                break;
+            }
+        }
+        if(board[1][1]!==''){
+            if (((board[0][0] === board[1][1]) && ( board[1][1] === board[2][2]))
+            || ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]))){
+                hasWon = true;
+                //console.log(hasWon, 'this is third check');
+                playerWon();
+            }
+        }
+    return hasWon;
+}        
 
-    // }
+//Display player won text
+function playerWon (){
+    if(hasWon){
+        endGame.className = 'show';
+    }
+}
 
-// }
+//Clear player won text
+function clearWon (){
+    endGame.className = 'hide';
+}
+   
+// Reset DOM
+resetButton.addEventListener('click',function(event){
+    for (let i=0; i<board.length; i++){
+        for(let j=0; j<board.length; j++){
+            board[i][j] = '';
+        }
+        //console.log(board);
+    }
 
-// function playerWon(){
-//     // logic for which player won
-//     endGame.classList = 'show';
-// }
+    for(let i = 0; i < cells.length; i++){
+        cells[i].innerText ='';
+
+    }  
+    clearWon();
+})
+        
+
 
